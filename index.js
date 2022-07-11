7313b9ab3efecc4e97596f5b11e53c0a8676910e
andor <latandor@gmail.com>
2022-07-11 11:14:26 -0400

    small changes

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const optionTab = document.querySelector(`option`)
 const funFact = document.querySelector(`#fun-fact`)
 const newCountryFormAdd = document.querySelector(`.form-div`)
 const newCountryFormRemove = document.querySelector(`#remove`)
+//const countryUpdate = document.querySelector(`#update`)
 const commentContainer = document.querySelector(`#paragraph-comment`)
 
+
+// select ID
 
 // fetching Public Api for country flag/ country picture/ some fun facts
 const fetchPublicApi = (country) => {
  fetch(`https://restcountries.com/v3.1/name/${country}`)
  .then(res => res.json())
- .then(data => data.forEach(country => {
-  postImage(country)
-  postFunFacts(country)
+ .then(data => data.forEach(countries => {
+  postImage(countries)
+  postFunFacts(countries)
   }))
 }
 
@@ -26,7 +29,8 @@ const fetchLocalHost = () => {
  .then(res => res.json())
  .then(data => {
   cleanupSelected()
-  data.forEach(visited => displaySelectOption(visited))
+  data.forEach(visited => {
+    displaySelectOption(visited)})
 })
 }
 
@@ -49,20 +53,23 @@ const postLocalHost = (country) => {
   (console.log(data))
 })
 }
-
+/*
 // Patch localhost 3000 edit the current comment or rating
-const patchLocalHost = (countryObj) => {
- fetch(`http://localhost:3000/visited/${countryObj.id}`, {
+const patchLocalHost = (ratingEdit, commentEdit) => {
+ fetch(`http://localhost:3000/visited/${id}`, {
   method: "PATCH", 
   headers: {
    "Content-type": "application/json"
   },
-  body: JSON.stringify(countryObj)
+  body: JSON.stringify({
+    rating: ratingEdit,
+    comment: commentEdit
+  })
  })
  .then(res => res.json())
  .then(data => console.log(data))
 }
-
+*/
 
 // delete from Localhost 3000 In order to refresh the list elements we need to call Get fetch request function.
 // since we are not using Promise line of code
@@ -79,8 +86,13 @@ const deleteLocalHost = (id) => {
   console.log(data)
 })
 }
-
-
+/*
+const displayCommentsSection = (commentParagraph) => {
+  let p = document.createElement(`p`)
+  p.innerText = commentParagraph.comment
+  commentContainer.append(p)
+}
+*/
 // adding image to the image element from public api
 const postImage = (post) => {
   let img = document.querySelector(`#comments`)
@@ -109,14 +121,14 @@ const displaySelectOption = (text) => {
   selectTab.append(option)
 
 }
-let selectId;
+//let selectId;
 // eventlistener for selecting and connecting the api picture
   selectTab.addEventListener(`change`, (e) => {
   let option2 = e.target.value
-  //let option3 = e.target.option.id
-  selectId = e.target
-  console.log(selectId)
-  fetchPublicApi(option2)  
+  //selectId = e.target
+  //console.log(selectId)
+  fetchPublicApi(option2) 
+  //fetchLocalHost()
  })
 
 
@@ -155,3 +167,43 @@ const cleanupSelected = () => {
 
 
 
+/*editRamenForm.addEventListener(`submit`, (e) => {
+  e.preventDefault() 
+  let ratingEdit = {
+    rating: e.target.rating.value
+  }
+  let commentEdit = {
+    comment: e.target[`new-comment`].value
+  }
+ setRamenRating(ratingEdit.rating)
+ setRamenComment(commentEdit.comment)
+ petchRamen(ratingEdit.rating, commentEdit.comment)
+})
+
+
+const petchRamen = (ratingEdit, commentEdit) => {
+  fetch(`http://localhost:3000/ramens/${currentId}`, {
+  method: "PATCH",
+  headers: {
+    "Content-type": "application/json"
+  },
+  body: JSON.stringify({
+    rating: ratingEdit,
+    comment: commentEdit
+  })
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+}*/
+
+/*
+countryUpdate.addEventListener(`click`, (e) => {
+
+  let ratingEdit = {
+    rating: e.target.rating.value
+  }
+  let commentEdit = {
+    comment: e.target.comment.value
+  }
+  patchLocalHost(ratingEdit.rating, commentEdit.comment)
+})*/
