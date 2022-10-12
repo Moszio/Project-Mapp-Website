90410385d6b1239bbbb08dcc09d42772343603b4
andor <latandor@gmail.com>
2022-10-12 08:54:13 -0400

    code cleanup

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//query Selectors and any additional variables
 const img = document.querySelector(`#comments`)
 const scrollDownBox = document.querySelector(`#down`)
 const selectTab = document.querySelector(`#img-select`)
@@ -6,35 +5,36 @@ const optionTab = document.querySelector(`option`)
 const funFact = document.querySelector(`#fun-fact`)
 const newCountryFormAdd = document.querySelector(`.form-div`)
 const newCountryFormRemove = document.querySelector(`#remove`)
-//const countryUpdate = document.querySelector(`#update`)
 const commentContainer = document.querySelector(`#paragraph-comment`)
-const a = document.querySelector(`a`).href = `#down`
-const icons = document.querySelector(`.icons`) 
+const a = (document.querySelector(`a`).href = `#down`)
+const icons = document.querySelector(`.icons`)
 const footerDiv = document.querySelector(`#footer`)
-const commentsDispla= document.querySelector(`#comments-display`)
+const commentsDispla = document.querySelector(`#comments-display`)
 
 // select ID
 
 // fetching Public Api for country flag/ country picture/ some fun facts
 const fetchPublicApi = (country) => {
- fetch(`https://restcountries.com/v3.1/name/${country}`)
- .then(res => res.json())
- .then(data => data.forEach(countries => {
-  postImage(countries)
-  postFunFacts(countries)
-  }))
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((res) => res.json())
+    .then((data) =>
+      data.forEach((countries) => {
+        postImage(countries)
+        postFunFacts(countries)
+      })
+    )
 }
-
 
 // fetch the Url from local host 3000. (Including country names / ratings / comments)
 const fetchLocalHost = () => {
- fetch(`http://localhost:3000/visited`)
- .then(res => res.json())
- .then(data => {
-  cleanupSelected()
-  data.forEach(visit => {
-    displaySelectOption(visit)})
-})
+  fetch(`http://localhost:3000/visited`)
+    .then((res) => res.json())
+    .then((data) => {
+      cleanupSelected()
+      data.forEach((visit) => {
+        displaySelectOption(visit)
+      })
+    })
 }
 
 /* This is to pin comments from countries to thhe website*/
@@ -50,47 +50,44 @@ const createElement = (id) => {
 }
 
 const fetchLocalHost2 = (id) => {
- fetch(`http://localhost:3000/visited/${id}`)
- .then(res => res.json())
- .then(data => createElement(data))
+  fetch(`http://localhost:3000/visited/${id}`)
+    .then((res) => res.json())
+    .then((data) => createElement(data))
 }
-
 
 fetchLocalHost()
 
-
 // Posting to Local Host 3000 any additional country with comments and ratings
 const postLocalHost = (country) => {
- return fetch(`http://localhost:3000/visited`, {
-  method: "POST",
-  headers: {
-   "Content-type": "application/json"
-  },
-  body: JSON.stringify(country)
- })
- .then(res => res.json())
- .then(data => {
-  fetchLocalHost();
-  (console.log(data))
-})
+  return fetch(`http://localhost:3000/visited`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(country),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      fetchLocalHost()
+      console.log(data)
+    })
 }
 
-
 // delete from Localhost 3000 In order to refresh the list elements we need to call Get fetch request function.
-// since we are not using Promise line of code
+
 const deleteLocalHost = (id) => {
- fetch(`http://localhost:3000/visited/${id}`, {
-  method: "DELETE", 
-  headers: {
-   "Content-type": "application/json"
-  }
- })
- .then(res => res.json())
- .then(data => {
-  fetchLocalHost()
-  fetchLocalHost2()
-  console.log(data)
-})
+  fetch(`http://localhost:3000/visited/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      fetchLocalHost()
+      fetchLocalHost2()
+      console.log(data)
+    })
 }
 
 // adding image to the image element from public api
@@ -105,11 +102,11 @@ const postFunFacts = (fact) => {
   const liBorder = document.createElement(`li`)
   const liPopulation = document.createElement(`li`)
   let border = fact.borders
-  funFact.innerHTML = ""
+  funFact.innerHTML = ''
   h3CapitalCity.innerText = `Capital City: ` + fact.capital
-  border === undefined ? ` ` : liBorder.innerText =  `Border: ` + fact.borders
-  liPopulation.innerText  = `Population: ` + fact.population
-  funFact.append(h3CapitalCity, liBorder, liPopulation, )
+  border === undefined ? ` ` : (liBorder.innerText = `Border: ` + fact.borders)
+  liPopulation.innerText = `Population: ` + fact.population
+  funFact.append(h3CapitalCity, liBorder, liPopulation)
   funFact.classList.remove(`hidden`)
 }
 // How to set a vaue to country name and have value of number too in order to delete and patch from local host
@@ -119,56 +116,48 @@ const displaySelectOption = (text) => {
   option.textContent = text.country
   option.id = text.id
   selectTab.append(option)
-
 }
 //let selectId;
 // eventlistener for selecting and connecting the api picture
-  selectTab.addEventListener(`change`, (e) => {
+selectTab.addEventListener(`change`, (e) => {
   let option2 = e.target.value
-   const options = selectTab.options;
-  const id = options[options.selectedIndex].id;
+  const options = selectTab.options
+  const id = options[options.selectedIndex].id
   //selectId = e.target
   //console.log(selectId)
-  fetchPublicApi(option2) 
+  fetchPublicApi(option2)
   fetchLocalHost2(id)
-
- })
-
+})
 
 // posting new countries to Localhost 3000
 newCountryFormAdd.addEventListener(`submit`, (e) => {
   e.preventDefault()
   let newCountry = {
-  country: e.target.country.value,
-  comment: e.target.comment.value,
-  rating: e.target.rating.value
-} 
-// remove all
+    country: e.target.country.value,
+    comment: e.target.comment.value,
+    rating: e.target.rating.value,
+  }
+  // remove all
   postLocalHost(newCountry)
   newCountryFormAdd.reset()
-
 })
 
 newCountryFormRemove.addEventListener(`click`, () => {
-  const options = selectTab.options;
-  const id = options[options.selectedIndex].id;
+  const options = selectTab.options
+  const id = options[options.selectedIndex].id
   deleteLocalHost(id)
 })
-
 
 icons.addEventListener(`mouseover`, () => {
   alert(`Not Today!`)
 })
 
-
 const cleanupSelected = () => {
   let img = document.querySelector(`#comments`)
-  selectTab.innerHTML = ""
-  img.src = ""
+  selectTab.innerHTML = ''
+  img.src = ''
   img.classList.add(`hidden`)
   const li = document.createElement(`li`)
-  funFact.innerHTML = ""
+  funFact.innerHTML = ''
   funFact.classList.add(`hidden`)
 }
-
-
